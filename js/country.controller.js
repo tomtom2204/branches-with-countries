43b48997af187a8a841d9eecedbfb5da8c55deb2
tomtom2204 <tomtom2204@gmail.com>
2024-10-06 18:02:08 +0300

    Add an <input>, a <button> & an empty <pre> tag to the html file. - Connect a script file. - Add an event handler to the button. When clicked, it will fetch data about the country in the input and call a render function to render it into the empty <pre> tag.

--- js/country.controller.js
+++ js/country.controller.js
@@ -7,3 +7,37 @@ function onGetCountryInfo() {
 function renderInfo(data) {
     console.log('Rendering...')
 }
+
+const elGetCountryBtn = document.querySelector('.get-country')
+
+// Add a click event listener
+elGetCountryBtn.addEventListener('click', function () {
+    // alert('Button was clicked!')
+    const elCountryName = document.querySelector('.country-name')
+    const countryName = elCountryName.value
+    console.log(countryName)
+    getCountryData(countryName)
+        .then(country => {
+            renderCountry(country)
+        })
+        .catch(err => {
+            console.error(`Encountered the following error while fetching data: ${err}`)
+        })
+})
+
+function renderCountry(country) {
+
+    const elCountryData = document.querySelector('.country-data')
+    console.log(country)
+    //TODO
+
+    const strHTMLs = `
+        country name: ${country[0].countryName}
+        population: ${country[0].population}
+        area: ${country[0].area}
+        neighbors: ${country[0].neighbors}
+        flag: ${country[0].flag}`
+
+    elCountryData.innerText = strHTMLs
+}
+
---
+++ js/services/countries.service.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const STORAGE_KEY = 'countryData'
+const CACHE_TIMEOUT = 1_000_000_000
+let gCountriesCache = loadFromStorage(STORAGE_KEY) || {}
+
+
+function getCountryData(countryName) {
+
+    if (gCountriesCache[countryName]) return Promise.resolve(gCountriesCache[countryName].data)
+
+    return axios.get(`https://restcountries.com/v3.1/name/${countryName}`)
+        .then(res => {
+            const countryData = res.data
+
+            gCountriesCache[countryName] = {
+                ts: Date.now(),
+                data: getCountry(countryData),
+            }
+            console.log('test: ', gCountriesCache[countryName].data)
+            saveToStorage('countryData', gCountriesCache)
+            return gCountriesCache[countryName].data
+        })
+}
+
+function getCountry(country) {
+
+    const data = country.map(country => {
+        return {
+            countryName: country.name.common,
+            population: country.population,
+            area: country.area,
+            neighbors: country.borders,
+            flag: country.flags.png
+        }
+    })
+    return data
+}
